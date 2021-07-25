2a6df67ebe86afb2d452c53f49372d7bf2a1218c
rishabhJ23 <rishabhjaiswal48@gmail.com>
2021-07-25 22:28:29 +0530

    did it without any video

---
+++ masterFile.py
@@ -0,0 +1,38 @@
+
+print("""
+Hi
+Its Easy to Learn Git.
+You can Run this file on your system.
+..................................................
+""")
+
+print("This is a change done on 17th July")
+#Write Something
+#x = kjnijvnwf
+
+print('''
+Also, copy below code on your Python IDE and RUN.
+Then submit the file back to the MASTER Link.
+..................................................
+''')
+
+###########################
+###########################
+###########################
+
+######### COPY ENTIRE SECTION BELOW #######
+
+print("TAERG gniod era uoY"[ : : -1])
+print("This is a simple maths power syntax")
+print(3 ** 3)
+
+print("Some more changes to this file...")
+#Some more changes
+
+
+
+import time
+time.sleep(5)
+
+
+
