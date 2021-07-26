960c43a8f188857a8c1da1c4eb2613fe8eb6ef3e
ayushi rawat <ayurawat07@gmail.com>
2021-07-26 21:21:17 +0530

    Add files via upload

---
+++ ayu/basics.py
@@ -0,0 +1,104 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "84510a4b",
+   "metadata": {
+    "scrolled": true
+   },
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "10*6\n",
+      "15625\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"10*6\")\n",
+    "\n",
+    "print(5**6)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "id": "c3aa0a65",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "50.0\n"
+     ]
+    }
+   ],
+   "source": [
+    "total_price=1000\n",
+    "n=20\n",
+    "\n",
+    "p=total_price /n\n",
+    "print(p)\n"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "a9cc5045",
+   "metadata": {},
+   "source": [
+    "\n",
+    "rules for var name"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "8a88c2c6",
+   "metadata": {},
+   "source": [
+    "#run cell= shift+enter\n",
+    "insertcell"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "651392be",
+   "metadata": {},
+   "source": [
+    "##"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "9261243d",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
